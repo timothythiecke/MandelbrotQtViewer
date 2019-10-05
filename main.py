3a908d03e33ee4e31926fe6d06d5b1602b08deb0
timothythiecke <timothy.thiecke@ugent.be>
2019-10-05 13:59:56 +0200

    Managed to get a simple version to draw on the screen

--- main.py
+++ main.py
@@ -12,20 +12,24 @@ def mandleInit(x, y, maxIterations):
     # and for y -1 -> 1
     
     xScaled = -2.5 + (3.5*(x/640))
-    yScaled = -1 + (2*(y/480)) # TODO: numbers need to be global
-
-    c = complex(xScaled, yScaled)
-
-    mandleRec(c, complex(0, 0), 0, maxIterations)
+    yScaled = -1 + (2*(y/480)) # TODO: numbers need to be global or shared
+    return mandleRec(complex(xScaled, yScaled), complex(0, 0), 0, maxIterations)
 
 def mandleRec(c, z, it, maxIterations):
     if it < maxIterations:
         tempZ = z*z
         tempZ += c
 
-        print(z)
-        mandleRec(c, tempZ, it + 1, maxIterations)
+        # Do square magnitude calculation to speed up
+        real = tempZ.real * tempZ.real 
+        im = tempZ.imag * tempZ.imag
 
+        if real + im > 4: # If the complex number is no longer bounded, we disregard it
+            return it
+        
+        return mandleRec(c, tempZ, it + 1, maxIterations)
+
+    return it
 
 
 class MandelbrotWindow(QMainWindow): # Python inheritance
@@ -38,18 +42,18 @@ class MandelbrotWindow(QMainWindow): # Python inheritance
         pixmap = QPixmap(640, 480).toImage()
         pixmap.fill(PyQt5.QtGui.QColor.fromRgb(255, 255, 255))
         
-        for i in range(300):
-            pixmap.setPixelColor(0, i, PyQt5.QtGui.QColor.fromRgb(255, 0, 0)) # First element is x, second is y
+        maxSteps = int(sys.argv[1])
+        for row in range(480):
+            for col in range(640):
+                steps = mandleInit(col, row, maxSteps)
+                pixmap.setPixelColor(col, row, PyQt5.QtGui.QColor.fromRgb(255*(1-(steps/maxSteps)), 0, 0)) # First element is x, second is y
+        
         #pixmap.setPixelColor(1, 1, PyQt5.QtGui.QColor.fromRgb(255, 255, 0))
         # ScanLine(0) return first pixel data
         #ptr = pixmap.scanLine(0) # Scanline
         #ptr2 = pixmap.bits()
         # bits() == scanLine(0)
         #print(ptr, ptr2)
-
-        mandleInit(0, 0, 5)
-
-
         # setPixelColor is less efficient, but easier to work with
 
         palette = QPalette()
